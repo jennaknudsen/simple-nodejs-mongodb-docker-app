6f4c549dba04ea740095947328df20a47296fd56
Jonas Knudsen <jonas.a.knudsen@gmail.com>
2021-11-05 00:17:57 -0600

    Minor bugfixes and cleanup

--- app.js
+++ app.js
@@ -9,8 +9,13 @@ const csvName = "classes.csv";
 
 const mongoClient = new mongodb.MongoClient(url);
 
+/**
+ * This class holds course information.
+ */
 class course {
     constructor(entry, prefix, courseNumber, courseName, credits) {
+        // Entry ticks up from 0 for each entry in the database; this lets us
+        // do stuff like "select all entries divisible by n
         this.dbEntry = entry;
         this.prefix = prefix;
         this.courseNumber = courseNumber;
@@ -35,20 +40,17 @@ async function loadCSV() {
     });
 
     // use await and read in each line from the readstream
-    let iteration = 0;
+    let iteration = -1;     // set iteration to -1 so that first entry in database is item 0
     for await (const line of rl) {
-        console.log("Line read: " + line);
-
         const entries = line.split(",");
 
         // Don't add bad lines to database
         if (entries.length < 4) {
-            console.log("Bad line: " + line);
             continue;
         }
 
         // don't add titles line of csv to database
-        if (iteration > 0) {
+        if (iteration >= 0) {
             allCourses.push(new course(iteration, entries[0], entries[1], entries[2], entries[3]));
         } else {
             console.log("Detect title line: " + line);
@@ -57,7 +59,7 @@ async function loadCSV() {
         iteration++;
     }
 
-    console.log("Done reading CSV");
+    console.log(`Done reading ${iteration} entries into CSV`);
 }
 
 /**
@@ -65,7 +67,7 @@ async function loadCSV() {
  */
 async function loadMongo() {
     try {
-        // use async-await
+        // wait for MongoDB connection to disconnect
         await mongoClient.connect();
         console.log("Connected to MongoDB");
 
@@ -78,18 +80,22 @@ async function loadMongo() {
 
         // gets number of documents in database
         const documentCount = await coursesCollection.countDocuments();
-
-        console.log("Number of documents counted: " + documentCount);
+        console.log("Number of documents counted in MongoDB: " + documentCount);
 
         // if database is empty, populate it with our csv
         if (documentCount === 0) {
             await loadCSV().then(() => coursesCollection.insertMany(allCourses));
-            console.log("Populated MongoDB with entries");
+            console.log("Populated MongoDB with entries from CSV");
+        } else {
+            console.log("MongoDB database already had entries, skipping reading CSV")
         }
     } catch (e) {
         console.log(e);
+    } finally {
+        // always close connection to mongo client afterwards
+        await mongoClient.close();
     }
 }
 
 // always load mongo
-loadMongo().then(() => console.log("Done opening Mongo"));
+loadMongo().then(() => console.log("Done opening MongoDB"));
